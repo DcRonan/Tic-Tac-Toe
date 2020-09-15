cd964bc8157925ad84db6667466ac747f4c06cf4
Bello S Babakolo <fleetbeekay@gmail.com>
2020-09-15 14:53:28 +0100

    Setup game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,29 @@
-# Display a welcome message.
-# Prompt the 2 players for their names
-# Assign pieces to the players like 'X' or 'O'. randomly.
-# first to play has 'X' second has 'O': Deciding who gets to start.
-#
-# The Game Loop.
-#
-#1. Display the board.
-#2. Prompt player to select a valid square (1 - 9).
-#3. if valid, update the board and switch turns
-#4. else prompt player to type-in a valid number.
-#4. go back to  1.
+puts 'WELCOME TO THIS TIC-TAC-TOE GAME: created by: '
+puts "Press 'ENTER' to begin"
+gets
 
+puts "\nPlayer 1:"
+player1 = gets.chomp
+
+puts "\nPlayer 2:"
+player2 = gets.chomp
+
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def display_board(board)
+  %(#{board[0]} | #{board[1]} | #{board[2]}
+----------
+#{board[3]} | #{board[4]} | #{board[5]}
+----------
+#{board[6]} | #{board[7]} | #{board[8]}
+  )
+end
+
+game_piece = 'X'
+
+loop do
+  puts display_board(board)
+  square_number = gets.chomp.to_i
+  board[square_number - 1] = game_piece
+  game_piece = game_piece == 'X' ? 'O' : 'X'
+end
