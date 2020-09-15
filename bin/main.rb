8ace67f48c54ab1d844dbf041716b33e5688cb68
Bello S Babakolo <fleetbeekay@gmail.com>
2020-09-15 13:49:45 +0100

    Create Tic-Tac-Toe Psuedocode.

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,13 @@
-puts 'Hello world!'
+# Display a welcome message.
+# Prompt the 2 players for their names
+# Assign pieces to the players like 'X' or 'O'. randomly.
+# first to play has 'X' second has 'O': Deciding who gets to start.
+#
+# The Game Loop.
+#
+#1. Display the board.
+#2. Prompt player to select a valid square (1 - 9).
+#3. if valid, update the board and switch turns
+#4. else prompt player to type-in a valid number.
+#4. go back to  1.
+
