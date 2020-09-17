a47f08995aa5c32a08e4e32a3a60ec7c32fdb672
Daniel Ronan <danielconnorronan@gmail.com>
2020-09-17 09:16:36 +0100

    moved game_loop

--- lib/game.rb
+++ lib/game.rb
@@ -5,27 +5,4 @@ class Game
     @players = { 'X' => player1, 'O' => player2 }
     @still_playing = true
   end
-
-  def game_loop
-    while @still_playing
-      puts "#{@players[@game_piece]}'s take your turn!"
-      puts @board.display
-      puts 'Select a Square:'
-      square_number = @board.pick_a_square do
-        puts 'Please choose a valid square, choose between 1-9 and that hasn\'t been selected!'
-      end
-      @board.set_a_square(square_number, @game_piece)
-      @game_piece = @game_piece == 'X' ? 'O' : 'X'
-      wining_status = @board.win_or_draw
-      break if %w[X O TIE].include?(wining_status)
-    end
-
-    puts @board.display
-    winner = @players[wining_status]
-    if winner
-      puts "#{winner} wins this round."
-    else
-      puts "It's a TIE"
-    end
-  end
 end
