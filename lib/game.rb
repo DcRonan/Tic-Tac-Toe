303409d6b7f1894b57c35e90898f56178eab49d6
Daniel Ronan <danielconnorronan@gmail.com>
2020-09-17 14:20:23 +0100

    deleted still_playing var and edit loop

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,11 @@
 class Game
-  attr_reader :board, :players, :still_playing
+  attr_reader :board, :players
   attr_accessor :game_piece
 
   def initialize(board, player1, player2)
     @game_piece = 'X'
     @board = board
     @players = { 'X' => player1, 'O' => player2 }
-    @still_playing = true
   end
 
   def current_player
