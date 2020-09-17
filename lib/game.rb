35c0d8d96a06a8de34e202a3472e7e7c74cb8c57
Bello S Babakolo <fleetbeekay@gmail.com>
2020-09-17 09:58:08 +0100

    Modularize the game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,19 @@
 class Game
+  attr_reader :board, :players, :still_playing
+  attr_accessor :game_piece
+
   def initialize(board, player1, player2)
     @game_piece = 'X'
     @board = board
     @players = { 'X' => player1, 'O' => player2 }
     @still_playing = true
+  end
+
+  def current_player
+    @players[@game_piece]
+  end
+
+  def win_draw
+    @board.win_or_draw
   end
 end
